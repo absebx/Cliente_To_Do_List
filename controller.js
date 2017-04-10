a5a52efac4a4f77d8c01c6c937987996e9e4cfba
asder707 <abn.sebick@gmail.com>
2017-04-10 17:54:43 -0300

    Se obtiene board con el usuario seleccionado, funcion getBoard en indexController, se modifica html en forma temporal

--- controller.js
+++ controller.js
@@ -1,8 +1,9 @@
 angular.module("indexApp",[])
   //index controller
   .controller("indexController",function($scope,$http,$location){
-    $scope.name="Abner";
+    //datos seleccionados
     $scope.selectedUser={};
+    $scope.selectedBoard={};
     //obtener usuarios de la base de datos
     $http.get("http://localhost:27697/api/users")
       .then(function(data){
@@ -10,3 +11,17 @@ angular.module("indexApp",[])
       },function(err){
         console.log(err);
       });
+
+    //obtener board del usuario
+    $scope.getBoard = function(user){
+      $scope.selectedUser = user;
+      //llamar a api por el board
+      $http.get("http://localhost:27697/api/boards/"+user.Id)
+        .then(function(data){
+          //seleccionar board
+          $scope.selectedBoard=data.data;
+        },function(err){
+          console.log(err);
+        });
+    }
+  });
